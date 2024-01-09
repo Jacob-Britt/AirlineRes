0fcbe22d79e1525440a3fb6f2e585382fd67c54c
Jacob-Britt <w0473727@nscc.ca>
2024-01-09 13:58:46 -0400

    Add project files.

---
+++ AirlineRes/main.cpp
@@ -0,0 +1,18 @@
+//airline reservation system capasity 10 seats 
+
+#include <iostream>
+
+using namespace std;
+
+//struct for passenger seat level, econmy or first class, first, birthdate, last name , seat number - jacob
+
+
+//functions
+
+//menu function - logan
+//add new passenger check for seat availability if not print message saying flight is full add to waitlist if user want to be added to second class instead if seat avalible  - danny
+//waitlist function - logan
+//print boarding pass to txt file BoardingPass.txt - jacob
+//function to make sure seat is not taken - danny
+
+
